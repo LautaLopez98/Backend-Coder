2cf2646854b31edbf279f2bfb615a7c207881e8f
LautaLopez98 <laueze1998@gmail.com>
2024-07-02 23:13:07 -0300

    Desafio completo

--- src/routes/CartRoutes.js
+++ src/routes/CartRoutes.js
@@ -6,9 +6,9 @@ export const router = Router();
 router.get("/", CartController.getCarts);
 router.get("/:cid", CartController.getCartById)
 router.post("/", CartController.createCart);
-router.post("/:cid/purchase", CartController.purchaseCart)
-router.post('/:cid/product/:pid',  CartController.addToCart);
-router.delete("/:cid/products/:pid", CartController.deleteProductInCart);
-router.put("/:cid",  CartController.updateCart);
-router.put("/:cid/products/:pid",  CartController.updateProductInCart);
-router.delete("/:cid",  CartController.deleteProducts);
+router.post("/:cid/purchase", auth(["user"]), CartController.purchaseCart)
+router.post('/:cid/product/:pid', auth(["user"]), CartController.addToCart);
+router.delete("/:cid/products/:pid", auth(["user"]), CartController.deleteProductInCart);
+router.put("/:cid", auth(["admin", "user"]), CartController.updateCart);
+router.put("/:cid/products/:pid", auth(["admin", "user"]), CartController.updateProductInCart);
+router.delete("/:cid", auth(["admin", "user"]), CartController.deleteProducts);
